b258c408fda88a330a2bdb65d676cc7c6b1b84f5
yugamauludi <yugamauludi98@gmail.com>
2023-05-19 09:26:45 +0700

    fixing bugs

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = 3000
 const router = require('./routers')
 const cors = require('cors') 
 
