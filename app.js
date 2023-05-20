b678af625546fab79ce3724c7bc21ec503a1669e
yugamauludi <yugamauludi98@gmail.com>
2023-05-20 10:23:20 +0700

    set up for deploy

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const router = require('./routers')
 const cors = require('cors') 
 
